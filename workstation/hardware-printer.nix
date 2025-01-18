702e60c5209fc25abace493e847b3111f07c258b
Jelly Terra <jellyterra@proton.me>
2025-01-18 20:57:19 +0800

    2025.01.18

--- workstation/hardware-printer.nix
+++ workstation/hardware-printer.nix
@@ -3,16 +3,13 @@
 { config, pkgs, lib, ... }: {
 
 services.printing.enable = true;
-services.printing.drivers = with pkgs; [
-	(writeTextDir "share/cups/model/EPSON_L130_Series.ppd" (builtins.readFile "/opt/ppd/EPSON_L130_Series.ppd"))
-];
 
 hardware.printers.ensureDefaultPrinter = "EPSON_L130_Series";
 hardware.printers.ensurePrinters = [
 	{
 		name = "EPSON_L130_Series";
-		deviceUri = "http://[::1]:631/printers/EPSON_L130_Series";
-		model = "EPSON_L130_Series.ppd";
+		deviceUri = "ipp://[::1]/printers/EPSON_L130_Series";
+		model = "everywhere";
 	}
 ];
 
